160f921b569f84663a7bc6a264f1ddc9d349b7f5
andyBzg <andr.bzg@gmail.com>
2023-02-18 13:14:08 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,79 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vertex first = new Vertex(null, "А");
+        Vertex second = new Vertex(null, "Б");
+        Vertex third = new Vertex(null, "В");
+        Vertex fourth = new Vertex(null, "Г");
+        Vertex fifth = new Vertex(null, "Д");
+        Vertex sixth = new Vertex(null, "Е");
+        Vertex seventh = new Vertex(null, "Ж");
+        Vertex eighth = new Vertex(null, "З");
+        Vertex ninth = new Vertex(null, "И");
+        Vertex tenth = new Vertex(null, "К");
+
+        List<Vertex> linksFirst = new ArrayList<>();
+        linksFirst.add(second);
+        linksFirst.add(third);
+        linksFirst.add(fourth);
+        linksFirst.add(fifth);
+        first.setLinks(linksFirst);
+
+        List<Vertex> linksSecond = new ArrayList<>();
+        linksSecond.add(third);
+        linksSecond.add(sixth);
+        second.setLinks(linksSecond);
+
+        List<Vertex> linksThird = new ArrayList<>();
+        linksThird.add(seventh);
+        third.setLinks(linksThird);
+
+        List<Vertex> linksFourth = new ArrayList<>();
+        linksFourth.add(eighth);
+        fourth.setLinks(linksFourth);
+
+        List<Vertex> linksFifth = new ArrayList<>();
+        linksFifth.add(fourth);
+        linksFifth.add(ninth);
+        fifth.setLinks(linksFifth);
+
+        List<Vertex> linksSixth = new ArrayList<>();
+        linksSixth.add(tenth);
+        sixth.setLinks(linksSixth);
+
+        List<Vertex> linksSeventh = new ArrayList<>();
+        linksSeventh.add(eighth);
+        linksSeventh.add(tenth);
+        seventh.setLinks(linksSeventh);
+
+        List<Vertex> linksEighth = new ArrayList<>();
+        linksEighth.add(tenth);
+        eighth.setLinks(linksEighth);
+
+        List<Vertex> linksNinth = new ArrayList<>();
+        linksNinth.add(tenth);
+        ninth.setLinks(linksNinth);
+
+
+        List<Vertex> vertices = new ArrayList<>();
+        vertices.add(first);
+        vertices.add(second);
+        vertices.add(third);
+        vertices.add(fourth);
+        vertices.add(fifth);
+        vertices.add(sixth);
+        vertices.add(seventh);
+        vertices.add(eighth);
+        vertices.add(ninth);
+        vertices.add(tenth);
+
+
+        for (Vertex vertex : vertices) {
+            System.out.println(vertex + " " + vertex.getLinks());
+        }
+
+    }
 }
--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Vertex {
+import java.util.List;
+
+public class Vertex {
+
+    private List<Vertex> links;
+    private String value;
+
+
+    public Vertex(List<Vertex> links, String value) {
+        this.links = links;
+        this.value = value;
+    }
+
+
+    public void setLinks(List<Vertex> links) {
+        this.links = links;
+    }
+
+    public List<Vertex> getLinks() {
+        return links;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
